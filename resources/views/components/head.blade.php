97402f9e2cd17581233cd0d9e202eee3143b7b35
OA <tech@olaarowolo.com>
2025-04-22 18:25:32 +0100

    updated og title to '...your trusted partner'

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>OA Tutors | ...trusted partners </title>
+    <title>OA Tutors | ...your trusted partner </title>
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="assets\img\oa-tutors-logo-blue-CF.jpg" />
     <link rel="icon" type="image/png" sizes="32x32" href="assets\img\oa-tutors-logo-blue-CF.jpg" />
